66cbfa30914876a08076b64b60adaa6715bfcc26
Lane Shetron <laneshetron@Lanes-Mac-mini.local>
2013-12-08 05:54:00 -0500

    finished keyboard hiding and added signin call to graffiti; authentication does not work yet

--- Wire/loginViewController.m
+++ Wire/loginViewController.m
@@ -39,25 +39,55 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-/*
-- (IBAction)backGroundTouched:(id)sender
-{
-    [sender resignFirstResponder];
-}
 
-- (IBAction)textFieldReturn:(id)sender
-{
-    [sender resignFirstResponder];
-} */
+/* Hide keyboard on return/background press */
 - (BOOL)textFieldShouldReturn: (UITextField *)textField
 {
     [textField resignFirstResponder];
     return NO;
 }
 
-- (IBAction)buttonTapped:(UIButton *) sender
+- (IBAction)touchBackground:(id)sender {
+    [uname resignFirstResponder];
+    [upass resignFirstResponder];
+}
+/****/
+
+- (IBAction)touchDown:(id)sender {
+    // send login request to graffiti
+    NSString *post = [NSString stringWithFormat:@"wire=wire&wire_user=%@&wire_pass=%@", uname.text, upass.text];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+    NSMutableURLRequest *loginRequest = [[NSMutableURLRequest alloc] init];
+    [loginRequest setURL:[NSURL URLWithString:@"http://graffiti.im/index.php"]];
+    [loginRequest setHTTPMethod:@"POST"];
+    [loginRequest setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [loginRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [loginRequest setHTTPBody:postData];
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:loginRequest delegate:self];
+    self.connection = conn;
+    [conn start]; // initiate connection
+    NSLog(@"login request sent");
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
-    
+    [self.response appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"%@", error);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSString *responseStr = [[NSString alloc] initWithData:self.response encoding:NSUTF8StringEncoding];
+    //int responseInt;
+    //[self.response getBytes:&responseInt length:sizeof(responseInt)];
+    //responseInt = CFSwapInt32BigToHost(responseInt);
+    //NSLog(@"%@", responseStr);
+    NSLog(@"%@", self.response);
 }
 
 @end
